144d3085968df75483d76ea912ae7f483364a9e3
Marcos Rocha <ccmarcos1@gmail.com>
2022-07-20 21:10:28 -0300

    v0.0

---
+++ MVCMeuSistema/Controllers/CadastroController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMeuSistema.Controllers
+{
+    public class CadastroController : Controller
+    {
+        // GET: Cadastro
+        public ActionResult Produto()
+        {
+            return View();
+        }
+        public ActionResult Cliente()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCMeuSistema/Models/Produtos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMeuSistema.Models
+{
+    public class Produtos
+    {
+        private readonly static string _conn = @"Data Source=.;Initial Catalog=devdb;Persist Security Info=True;User ID=admin;Password=123";
+
+            public int ID { get; set; }
+            public string NOME { get; set; }
+            public string VALOR { get; set; }
+            public int QUANTIDADE { get; set; }
+            public DateTime DATAHORA { get; set; }
+            
+    }
+}
